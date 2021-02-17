09c537a57a4f1db3daa601daba8c7c7949a072cc
Marcel Pekacki <marcel.x.pekacki@gsk.com>
2021-02-17 23:19:17 +0100

    npm update

---
+++ docs/precache-manifest.9c597535051fd44c77f82230fc6a89c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bed115a21cd2678adf0d5891c8313a",
+    "url": "/cooltimer/index.html"
+  },
+  {
+    "revision": "b5db2894491fd0ded57d",
+    "url": "/cooltimer/static/css/2.0caf9358.chunk.css"
+  },
+  {
+    "revision": "aa09f41b02ab9fae1e39",
+    "url": "/cooltimer/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "b5db2894491fd0ded57d",
+    "url": "/cooltimer/static/js/2.1524597b.chunk.js"
+  },
+  {
+    "revision": "e248b2b7c6d378c139e8646fff15f165",
+    "url": "/cooltimer/static/js/2.1524597b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa09f41b02ab9fae1e39",
+    "url": "/cooltimer/static/js/main.21aa2da5.chunk.js"
+  },
+  {
+    "revision": "6bb03c2b2fe7d8a875fe",
+    "url": "/cooltimer/static/js/runtime-main.6509e71a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cooltimer/precache-manifest.347594c426b53d224698c56ec477bd21.js"
+  "/cooltimer/precache-manifest.9c597535051fd44c77f82230fc6a89c9.js"
 );
 
 self.addEventListener('message', (event) => {
