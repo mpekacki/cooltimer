cd57e437f4aca3cb1c7e321906f59095e4052d74
Marcel Pekacki <mpekacki@craftware.biz>
2021-05-24 21:47:10 +0200

    event title tooltip

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ class App extends React.Component {
     }
     let newEvents = [...this.state.events, newEvent];
     if (newEvents.length > 1 && newEvents[newEvents.length - 1].isWork === newEvents[newEvents.length - 2].isWork
-      && newEvents[newEvents.length - 2].end.getTime() === newEvents[newEvents.length - 1].start.getTime() 
+      && newEvents[newEvents.length - 2].end.getTime() === newEvents[newEvents.length - 1].start.getTime()
       && newEvents[newEvents.length - 1].task === newEvents[newEvents.length - 2].task) {
       newEvents = newEvents.slice(0, newEvents.length - 1);
       newEvents[newEvents.length - 1].end = new Date(event.end);
@@ -154,7 +154,7 @@ class App extends React.Component {
       start: this.state.timerStartedAt,
       end: end
     });
-  
+
     this.setStateAndStorage({
       timerStartedAt: end,
       timerStartedWithSeconds: this.state.timerSeconds,
@@ -180,7 +180,7 @@ class App extends React.Component {
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        
+
         <div className="container">
           <div className="row">
             <div className="col-sm offset-sm-11">
@@ -221,12 +221,14 @@ class App extends React.Component {
             <SimpleTaskManager onTaskCreate={this.handleTaskCreated} onTaskSelected={this.handleTaskSelected} tasks={this.state.tasks} selectedTask={this.state.selectedTask} />
           </div>
           <div className="row mb-3">
-            <TaskTimes events={this.state.events} eventsTimestamp={this.state.eventsTimestamp}/>
+            <TaskTimes events={this.state.events} eventsTimestamp={this.state.eventsTimestamp} />
           </div>
           <div className="card card-body">
             <FullCalendar events={this.state.events} plugins={[timeGridPlugin, dayGridMonth, listPlugin]} initialView="timeGridWeek" headerToolbar={
               { right: 'today prev,next dayGridMonth,timeGridWeek,timeGridDay listWeek' }
-            } slotDuration='00:10:00' />
+            } slotDuration='00:10:00' eventDidMount={function (event) {
+              event.el.title = event.event.title;
+            }} />
           </div>
         </div>
       </div>
