4f7a7ad074070c3b69c0ab04c3338582c0b154c3
Marcel Pekacki <mpekacki@craftware.biz>
2021-06-09 10:01:17 +0200

    links moved

--- src/App.js
+++ src/App.js
@@ -204,16 +204,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <Helmet>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-            crossorigin="anonymous"
-          />
-
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-
           <title>Timer</title>
         </Helmet>
         <Container>
