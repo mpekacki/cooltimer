aaf9c37a3974fee0114d64f8c9460fcf573da2fa
mpekacki <mpekacki@craftware.biz>
2021-08-17 18:55:27 +0200

    report web vitals

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import DefaultSettings from "./DefaultSettings";
 import BrowserNotifications from "./BrowserNotifications";
 import LocalStorage from "./LocalStorage";
@@ -22,3 +23,8 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
