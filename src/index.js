09a3bee65735007f3915bc9521acd09989fdebf4
mpekacki <mpekacki@craftware.biz>
2021-08-17 18:28:24 +0200

    new service worker from CRA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "@fontsource/signika";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import DefaultSettings from "./DefaultSettings";
 import BrowserNotifications from "./BrowserNotifications";
 import LocalStorage from "./LocalStorage";
@@ -20,5 +20,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
